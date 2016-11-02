5f945d392666dd4322181eefbe38dfffe34511e3
kracken9500 <evanm831@gmail.com>
2016-11-01 20:46:16 -0500

    Added new class Map with key variables

---
+++ GameOne/src/com/map/Map.java
@@ -0,0 +1,12 @@
+package com.map;
+
+public class Map {
+	public static final int x = 100;
+	public static final int y = 100;
+	public int[] map = new int[x * y];
+	
+	public static final int bl = 5;
+	public static final int bo = 5;
+	
+	public static final int t = 10;
+}
