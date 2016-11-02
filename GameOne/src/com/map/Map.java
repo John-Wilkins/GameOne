3dea6fb6f58229390be2b6d6909a0d6b91bf972e
kracken9500 <evanm831@gmail.com>
2016-11-01 20:59:42 -0500

    Added comments to variables in Map

--- GameOne/src/com/map/Map.java
+++ GameOne/src/com/map/Map.java
@@ -1,12 +1,18 @@
 package com.map;
 
 public class Map {
+	//x is the horizontal resolution of the map
+	//y is the vertical resolution of the map
+	//map is the map array
 	public static final int x = 100;
 	public static final int y = 100;
 	public int[] map = new int[x * y];
 	
+	//bl is the number of random bits generated for land seeds
+	//bo is the number of random bits generated for ocean seeds
 	public static final int bl = 5;
 	public static final int bo = 5;
 	
+	//t is the number of traits (including land/ocean) in Array 2
 	public static final int t = 10;
 }
